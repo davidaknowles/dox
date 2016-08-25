4b3a3816377a9f3b74bdb5205b24ca07f1c480dc
John Blischak <jdblischak@gmail.com>
2016-08-25 11:41:42 -0500

    Remove code/data from old analyses.
    1. Analysis of in-house flow cell on CRI Tarbell. Removing because
    we ended up sequencing the rest at the core, so the batch effects
    are not worth the effort.
    
    2. Analysis of Burridge et al. with kallisto. This was a quick
    initial analysis. Now it needs to be run through the same
    pipeline as the rest of our data.

--- code/submit-fastqc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run from $dox
-
-for FQ in `ls fastq/*`
-do
-  ID=`basename ${FQ%.fastq.gz}`
-  echo "run-fastqc.sh $FQ" | qsub -N fastqc.$ID -S /bin/bash -l walltime=1:00:00 -l nodes=1:ppn=4 -l mem=4gb -e $HOME -V -d . -j eo 
-done
--- code/submit-samtools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run from $dox
-
-for BAM in `ls bam/*bam`
-do
-  ID=`basename ${BAM%.bam}`
-  echo "run-samtools.sh $BAM" | qsub -N samtools.$ID -S /bin/bash -l walltime=1:00:00 -l nodes=1:ppn=4 -l mem=8gb -o $HOME -e $HOME -V -d .
-done
