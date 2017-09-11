89f4e2eb37318ca88459972fd807d8657c935b26
davidaknowles <knowles84@gmail.com>
2017-09-11 12:10:27 -0700

    a

--- code/submit_iqtl.sh
+++ code/submit_iqtl.sh
@@ -3,19 +3,22 @@
 # Submit per chromosome interaction eQTL scans
 # 
 
+echo "Data dir: $DOX_DATA"
+
 if [ -z "$3" ]
 then
     echo "Not enough arguments supplied"
     echo "Usage: submit_iqtl.sh normalization permutation_approach cisdist"
-    echo "Data: $DOX_DATA"
     exit 1
 fi
 
+cisdist=$(printf "%g" "$3")
 for CHROMID in {1..22}; do
-  resfile=${DOX_DATA}/sqtl_$1_$2/chr${CHROMID}.txt.gz
+  resfile=${DOX_DATA}/panama_$1_$2_$cisdist/chr${CHROMID}.txt.gz
   if [ ! -f $resfile ]; then
       #qsub -l nodes=1:ppn=16 -d . -q daglab -v CHROM=chr$CHROMID,NORM=$1,PERM=$2 -N lrt$CHROMID run_iqtl.sh
-      sbatch  -J sqtl_$CHROMID run_iqtl.batch $1 chr$CHROMID $2 $3
+      echo $resfile not found
+      sbatch  -J iqtl_$CHROMID run_iqtl.batch $1 chr$CHROMID $2 $3
   else
       echo $resfile exists
   fi
