7ac82ca2c0e4943c25b4d2dbf769497f9333338c
David A. Knowles <knowles84@gmail.com>
2017-09-18 23:56:09 -0700

    mapping bug with file handles

--- code/map_interaction_qtl.R
+++ code/map_interaction_qtl.R
@@ -2,12 +2,12 @@ require(rstan)
 
 ## Wrapper to supress extraneous stdout from rstan::optimizing
 stan_optimizing_wrapper = function(...) {
-  zz <- file( "/dev/null", open = "wt")
-  sink(zz)
-  sink(zz, type = "message")
+  #zz <- file( "/dev/null", open = "wt")
+  #sink(zz)
+  #sink(zz, type = "message")
   res=optimizing(...)
-  sink(type="message")
-  sink()
+  #sink(type="message")
+  #sink()
   res
 }
 
@@ -37,8 +37,14 @@ map_interaction_qtl = function(input, genotype, geneloc, snploc, anno, sample_ke
   panama_test=stan_model("panama_test.stan")
 
   errorhandling=if (interactive()) 'stop' else 'remove'
+  print(errorhandling)
+  #stopifnot(FALSE)
   
-  foreach(gene=genes, .errorhandling=errorhandling, .combine = bind_rows) %do% {
+  zz <- file( "/dev/null", open = "wt")
+  sink(zz)
+  sink(zz, type = "message")
+  
+  all_results = foreach(gene=genes, .errorhandling=errorhandling, .combine = bind_rows) %do% {
     
     check_fn=paste0(checkpoint_dir,gene,".txt.gz")
     if (file.exists(check_fn)) {
@@ -52,26 +58,26 @@ map_interaction_qtl = function(input, genotype, geneloc, snploc, anno, sample_ke
     cat(gene,length(cis_snps)," cis snps\n")
     
     if (length(cis_snps)==0) return(NULL)
-    print(1)
+    #print(1)
     y=input[gene,] %>% as.numeric
     y=y-mean(y)
     
     imp_geno=easy_impute(genotype[cis_snps,,drop=F])
-    print(2)
-    if (permutation_approach=="permute") colnames(imp_geno)=colnames(imp_geno)[ sample(ncol(imp_geno),ncol(imp_geno)) ]
+    #print(2)
+    #if (permutation_approach=="permute") colnames(imp_geno)=colnames(imp_geno)[ sample(ncol(imp_geno),ncol(imp_geno)) ]
     # cis_snp=as.character(cis_snps)[1]
-    print("2a")    
+    #print("2a")    
     data=list(N=N,U_transpose_x=t(eigen_sample_kernel$vectors) %*% no_geno,P=ncol(no_geno), U_transpose_y=t(eigen_sample_kernel$vectors) %*% y %>% as.numeric, lambda=eigen_sample_kernel$values)
-    print("2b")
+    #print("2b")
     init=list(sigma2=0.1, sigma2_k=1.0, beta=lm(y ~ no_geno - 1) %>% coef )
-    print(3)
+    #print(3)
     fit_no_geno=stan_optimizing_wrapper(panama_test, data, init=init, as_vector=F)
-    print("Here")
-    gene_results = foreach(cis_snp=cis_snps, .errorhandling=errorhandling, .combine = bind_rows) %do% {
-    	print(cis_snp)
+   # print("Here")
+    gene_results = foreach(cis_snp=cis_snps, .errorhandling=errorhandling, .combine = bind_rows) %dopar% {
+    	#print(cis_snp)
       geno=imp_geno[cis_snp,anno$findiv]
       if (sum(imp_geno[cis_snp,]) < 5.0) {
-      	 print("Too few minor alleles")
+      	 #print("Too few minor alleles")
 	 return(NULL)
  	 }
       
@@ -116,13 +122,18 @@ map_interaction_qtl = function(input, genotype, geneloc, snploc, anno, sample_ke
       
       res
     }
-  print("Saving results")
-  checkpoint_file= gzfile( check_fn,"w")
-  gene_results %>% format(digits=5) %>% write.table(checkpoint_file, quote = F, row.names = F, col.names = T, sep="\t")
-  close(checkpoint_file)
-  
-  gene_results
+    print("Saving results")
+    checkpoint_file= gzfile( check_fn,"w")
+    gene_results %>% format(digits=5) %>% write.table(checkpoint_file, quote = F, row.names = F, col.names = T, sep="\t")
+    close(checkpoint_file)
+    
+    gene_results
   }
+  
+  sink(type="message")
+  sink()
+  
+  all_results
 }
 
 # Could just zcat everything into one file rather than bind_rows
