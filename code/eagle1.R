6bb0b5a411e8023f9e4f1ed8655dc63d7f25ed98
davidaknowles <knowles84@gmail.com>
2018-04-04 18:34:57 -0700

    eagle fixes

--- code/eagle1.R
+++ code/eagle1.R
@@ -1,11 +1,12 @@
 require(data.table)
-require(dplyr)
+require(tidyverse)
+require(magrittr)
 require(foreach)
 source("utils.R")
 source("load_data.R")
 require(rstan)
 require(doMC)
-registerDoMC(16)
+registerDoMC(20)
 
 cisdist=1e5
 
@@ -17,16 +18,18 @@ if (interactive()) {
 
 cat("Chrom:",which_chr,"\n")
 
-dat=fread("zcat < ../data/ase.txt.gz", data.table=F) %>%
+dat=fread(paste0("zcat < ",DATADIR,"/ase.txt.gz"), data.table=F) %>%
   mutate(snp=paste(chr,pos,sep=":"))  %>% 
   separate(sample, c("cell_line","cond"), sep="_")
 
-geneloc=read.table(paste0(DATADIR,"genelocGRCh38.txt.gz"),header=T,stringsAsFactors = F)
+geneloc=read.table(paste0(DATADIR,"genelocGRCh38.txt.gz"),header=T,stringsAsFactors = F)  %>% 
+  filter(chr==which_chr)
+
 rownames(geneloc)=geneloc$geneid
 
 bb_stan=stan_model("bb.stan")
 
-anno_findiv=read.table("../data/annotation_findiv.txt", header=T, stringsAsFactors = F) %>% 
+anno_findiv=read.table(paste0(DATADIR,"annotation_findiv.txt"), header=T, stringsAsFactors = F) %>% 
   select(cell_line, findiv) %>%
   distinct() %>% 
   left_join(sample_anno %>%
@@ -38,7 +41,7 @@ anno_findiv=read.table("../data/annotation_findiv.txt", header=T, stringsAsFacto
 phased_types=c("0|0","0|1","1|0","1|1")
 phased_hets=c("0|1","1|0")
 
-phased=fread(paste0("zcat < ../data/phased_genotypes/dox-hg38-",which_chr,".vcf.gz"), data.table=F, skip = "#CHROM", header=T)
+phased=fread(paste0("zcat < ",DATADIR,"/phased_genotypes/dox-hg38-",which_chr,".vcf.gz"), data.table=F, skip = "#CHROM", header=T)
 #phased=fread("zcat < ../data/dox-hg38.vcf.gz", data.table=F, skip = "#CHROM", header=T)
 
 colnames(phased)[1]="CHROM"
@@ -68,26 +71,21 @@ dir.create( outputdir, showWarnings = F )
 checkpoint_dir=paste0(outputdir,which_chr,"/")
 dir.create(checkpoint_dir, recursive = T, showWarnings = F)
 
-foreach(gene=unique(geneloc$geneid), .errorhandling = "remove", .combine = bind_rows) %do% {
-
+allres=foreach(gene=unique(geneloc$geneid), .errorhandling = "stop", .combine = bind_rows) %do% {
+  print(gene)
   if (!is.null(checkpoint_dir)){
     check_fn=paste0(checkpoint_dir,gene,".txt.gz")
     if (file.exists(check_fn)) {
-      return(read.table(check_fn, header=T, stringsAsFactors = F, sep="\t"))
+      return(read.table(check_fn, header=T, stringsAsFactors = F, sep="\t") %>% mutate(gene=gene))
     }
   }
   gene_meta = geneloc %>% filter(geneid == gene)
   
-  #phased_exonic = phased %>% filter( POS > gene_meta$left, POS < gene_meta$right )
-  
   ase_dat = dat_chr %>% filter( pos > gene_meta$left, pos < gene_meta$right ) 
   
   allelic_count_total=sum(ase_dat$y+ase_dat$r)
-  if (allelic_count_total < 5000) {
-    cat("Allelic total count only ", allelic_count_total,", skipping...\n")
-    return(NULL)
-  }
-  
+  cat("Allelic total count ", allelic_count_total, "\n")
+  if (allelic_count_total < 5000) return(NULL)
   if (nrow(ase_dat) < 20) return(NULL)
   
   cis_snps=phased %>% 
@@ -116,27 +114,27 @@ foreach(gene=unique(geneloc$geneid), .errorhandling = "remove", .combine = bind_
     
     x_full = if (length(unique(ase_temp$het_x)) > 1) model.matrix( ~ het_x + cond:het_x, data=ase_temp ) else model.matrix( ~ cond, data=ase_temp ) # testing the exonic SNP itself
     
-     stan_dat = list(N=nrow(x_full), P=ncol(x_full), x=x_full, ys=ase_temp$y, ns=ase_temp$y + ase_temp$r, concShape=1.001, concRate=0.001)
-     fit_full=if (det(t(x_full) %*% x_full) > 0) { optimizing(bb_stan, data=stan_dat)$value } else NA
-
-     x_null = if (length(unique(ase_temp$het_x)) > 1) model.matrix( ~ het_x , data=ase_temp ) else model.matrix( ~ 1, data=ase_temp )
-     stan_dat$x=x_null
-     stan_dat$N=nrow(x_null)
-     stan_dat$P=ncol(x_null)
-     fit_null=optimizing(bb_stan, data=stan_dat)$value
-     
-     x_0 = model.matrix( ~ 1, data=ase_temp )
-     stan_dat$x=x_0
-     stan_dat$N=nrow(x_0)
-     stan_dat$P=ncol(x_0)
-     fit_0=optimizing(bb_stan, data=stan_dat)$value
-     
-     #ase_temp %>% mutate(het=reg_geno %in% phased_hets, coverage=r+y, ar = r/coverage,in_phase=geno == reg_geno,  car=ifelse(in_phase,ar,1-ar)) %>% ggplot(aes( cond,car,size=coverage,shape=snp,col=het)) + geom_point() + ylim(0,1) + ggtitle(paste0(top_hit$gene," p=",format(pv,digits=3))) + scale_shape_manual(values=shape_values[seq_along(levels(to_plot$snp))], guide=F )   + geom_line(aes(group=interaction(dbgap,snp)),size=1,alpha=0.5)  + xlab("Dox concentration") + ylab("Phased allelic ratio")
-     
-     df=ncol(x_full) - ncol(x_null)
-     #lrt=2.0*(fit_full$value - fit_null$value)
-     # nlog10p=-pchisq(lrt, df, lower.tail = F, log.p = T)/log(10)
-     data.frame(gene=gene, snp=snp_pos, df=df, l0=fit_0, l1=fit_null, l2=fit_full)
+    stan_dat = list(N=nrow(x_full), P=ncol(x_full), x=x_full, ys=ase_temp$y, ns=ase_temp$y + ase_temp$r, concShape=1.001, concRate=0.001)
+    fit_full=if (det(t(x_full) %*% x_full) > 0) { optimizing(bb_stan, data=stan_dat)$value } else as.numeric(NA)
+    
+    x_null = if (length(unique(ase_temp$het_x)) > 1) model.matrix( ~ het_x , data=ase_temp ) else model.matrix( ~ 1, data=ase_temp )
+    stan_dat$x=x_null
+    stan_dat$N=nrow(x_null)
+    stan_dat$P=ncol(x_null)
+    fit_null=optimizing(bb_stan, data=stan_dat)$value
+    
+    x_0 = model.matrix( ~ 1, data=ase_temp )
+    stan_dat$x=x_0
+    stan_dat$N=nrow(x_0)
+    stan_dat$P=ncol(x_0)
+    fit_0=optimizing(bb_stan, data=stan_dat)$value
+    
+    # ase_temp %>% mutate(het=reg_geno %in% phased_hets, coverage=r+y, ar = r/coverage,in_phase=geno == reg_geno,  car=ifelse(in_phase,ar,1-ar)) %>% ggplot(aes( cond,car,size=coverage,shape=snp,col=het)) + geom_point() + ylim(0,1) + ggtitle(paste0(top_hit$gene," p=",format(pv,digits=3))) + scale_shape_manual(values=shape_values[seq_along(levels(to_plot$snp))], guide=F )   + geom_line(aes(group=interaction(dbgap,snp)),size=1,alpha=0.5)  + xlab("Dox concentration") + ylab("Phased allelic ratio")
+    
+    df=ncol(x_full) - ncol(x_null)
+    # lrt=2.0*(fit_full$value - fit_null$value)
+    # nlog10p=-pchisq(lrt, df, lower.tail = F, log.p = T)/log(10)
+    data.frame(snp=snp_pos, df=df, l0=fit_0, l1=fit_null, l2=fit_full, stringsAsFactors=F)
   }
   
   if (!is.null(checkpoint_dir)){
@@ -145,6 +143,10 @@ foreach(gene=unique(geneloc$geneid), .errorhandling = "remove", .combine = bind_
     gene_results %>% format(digits=5) %>% write.table(checkpoint_file, quote = F, row.names = F, col.names = T, sep="\t")
     close(checkpoint_file)
   }
-  gene_results
   
+  gene_results %>% mutate(gene=gene)
 }
+
+res_file= gzfile( paste0(outputdir,which_chr,".txt.gz"), "w" )
+allres %>% format(digits=5) %>% write.table(res_file, quote = F, row.names = F, col.names = T, sep="\t")
+close(res_file)
--- code/run_iqtl.sh
+++ code/run_iqtl.sh
@@ -4,9 +4,11 @@
 cd $PBS_O_WORKDIR
 
 echo Chromosome: $CHROM
-echo Normalization: $NORM
-echo Perm: $PERM
+#echo Normalization: $NORM
+#echo Perm: $PERM
 
 # Rscript run_iqtl_lrt.R $NORM $CHROM
 # Rscript panama_test.R $NORM $CHROM $PERM
+# Rscript panama_test_leaf.R $NORM $CHROM $PERM
+
 Rscript panama_test_leaf.R $NORM $CHROM $PERM
